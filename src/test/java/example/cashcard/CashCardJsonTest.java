a86bfd3362bcf4b1ae01c8eedc87f2211bb6bb7d
mabi0110 <biziewskamk@gmail.com>
2024-03-11 21:10:12 +0100

    initial commit

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
