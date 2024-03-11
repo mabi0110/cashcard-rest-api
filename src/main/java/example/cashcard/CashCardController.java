57296e36749f8abf84be60f94830a4d0ab161f5d
mabi0110 <biziewskamk@gmail.com>
2024-03-11 23:46:46 +0100

    add test for get method

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,18 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{id}")
+    private ResponseEntity<CashCard> findById() {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        return ResponseEntity.ok(cashCard);
+    }
+
+}
